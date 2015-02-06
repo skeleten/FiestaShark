8ff2de45308e2383ddb604fa6450e556c5403487
skeleten <skele@ymail.com>
2015-02-06 19:47:29 +0100

    some more reformatting

--- src/Program.cs
+++ src/Program.cs
@@ -5,54 +5,44 @@ using System.Reflection;
 using System.Windows.Forms;
 
 namespace MapleShark {
-	internal static class Program
-    {
-        [STAThread]
-        private static void Main(string[] pArgs)
-        {
-            RegisterFileAssociation(".msb", "MapleShark", "MapleShark Binary File", Assembly.GetExecutingAssembly().Location, string.Empty, 0);
-            RuntimeHost.Initialize();
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            _MainForm = new MainForm(pArgs);
-            Application.Run(_MainForm);
-        }
-        internal static string AssemblyVersion { get { return Assembly.GetExecutingAssembly().GetName().Version.ToString(); } }
-        internal static string AssemblyCopyright { get { return ((AssemblyCopyrightAttribute)Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false)[0]).Copyright; } }
+	internal static class Program {
+		[STAThread]
+		private static void Main(string[] pArgs) {
+			RegisterFileAssociation(".msb", "MapleShark", "MapleShark Binary File", Assembly.GetExecutingAssembly().Location, string.Empty, 0);
+			RuntimeHost.Initialize();
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			_MainForm = new MainForm(pArgs);
+			Application.Run(_MainForm);
+		}
+		internal static string AssemblyVersion { get { return Assembly.GetExecutingAssembly().GetName().Version.ToString(); } }
+		internal static string AssemblyCopyright { get { return ((AssemblyCopyrightAttribute)Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false)[0]).Copyright; } }
 
-        internal static MainForm MainForm { get { return _MainForm;  } }
+		internal static MainForm MainForm { get { return _MainForm; } }
 
 
-        private static MainForm _MainForm;
-        private static void RegisterFileAssociation(string pExtension, string pProgramId, string pDescription, string pEXE, string pIconPath, int pIconIndex)
-        {
-            try
-            {
-                if (pExtension.Length != 0)
-                {
-                    if (pExtension[0] != '.') pExtension = "." + pExtension;
+		private static MainForm _MainForm;
+		private static void RegisterFileAssociation(string pExtension, string pProgramId, string pDescription, string pEXE, string pIconPath, int pIconIndex) {
+			try {
+				if (pExtension.Length != 0) {
+					if (pExtension[0] != '.') pExtension = "." + pExtension;
 
-                    using (RegistryKey key = Registry.ClassesRoot.OpenSubKey(pExtension)) if (key == null) using (RegistryKey extKey = Registry.ClassesRoot.CreateSubKey(pExtension)) extKey.SetValue(string.Empty, pProgramId);
+					using (RegistryKey key = Registry.ClassesRoot.OpenSubKey(pExtension)) if (key == null) using (RegistryKey extKey = Registry.ClassesRoot.CreateSubKey(pExtension)) extKey.SetValue(string.Empty, pProgramId);
 
-                    using (RegistryKey extKey = Registry.ClassesRoot.OpenSubKey(pExtension))
-                    {
-                        using (RegistryKey key = extKey.OpenSubKey(pProgramId))
-                        {
-                            if (key == null)
-                            {
-                                using (RegistryKey progIdKey = Registry.ClassesRoot.CreateSubKey(pProgramId))
-                                {
-                                    progIdKey.SetValue(string.Empty, pDescription);
-                                    using (RegistryKey defaultIcon = progIdKey.CreateSubKey("DefaultIcon")) defaultIcon.SetValue(string.Empty, String.Format("\"{0}\",{1}", pIconPath, pIconIndex));
+					using (RegistryKey extKey = Registry.ClassesRoot.OpenSubKey(pExtension)) {
+						using (RegistryKey key = extKey.OpenSubKey(pProgramId)) {
+							if (key == null) {
+								using (RegistryKey progIdKey = Registry.ClassesRoot.CreateSubKey(pProgramId)) {
+									progIdKey.SetValue(string.Empty, pDescription);
+									using (RegistryKey defaultIcon = progIdKey.CreateSubKey("DefaultIcon")) defaultIcon.SetValue(string.Empty, String.Format("\"{0}\",{1}", pIconPath, pIconIndex));
 
-                                    using (RegistryKey command = progIdKey.CreateSubKey("shell\\open\\command")) command.SetValue(string.Empty, String.Format("\"{0}\" \"%1\"", pEXE));
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception) { }
-        }
-    }
+									using (RegistryKey command = progIdKey.CreateSubKey("shell\\open\\command")) command.SetValue(string.Empty, String.Format("\"{0}\" \"%1\"", pEXE));
+								}
+							}
+						}
+					}
+				}
+			} catch (Exception) { }
+		}
+	}
 }
--- src/StructureNode.cs
+++ src/StructureNode.cs
@@ -1,18 +1,16 @@
 ﻿using System.Windows.Forms;
 
 namespace MapleShark {
-	public class StructureNode : TreeNode
-    {
-        public byte[] Buffer;
-        public int Cursor;
-        public int Length;
+	public class StructureNode : TreeNode {
+		public byte[] Buffer;
+		public int Cursor;
+		public int Length;
 
-        public StructureNode(string pDisplay, byte[] pBuffer, int pCursor, int pLength)
-            : base(pDisplay)
-        {
-            Buffer = pBuffer;
-            Cursor = pCursor;
-            Length = pLength;
-        }
-    }
+		public StructureNode(string pDisplay, byte[] pBuffer, int pCursor, int pLength)
+				: base(pDisplay) {
+			Buffer = pBuffer;
+			Cursor = pCursor;
+			Length = pLength;
+		}
+	}
 }
